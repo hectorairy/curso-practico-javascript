eaea32a264360149962caf1aad51571dfce121d3
Héctor Airy <hola@hectorairy.com>
2022-07-30 17:33:10 -0500

    write html elements using javascript

--- index.js
+++ index.js
@@ -13,3 +13,29 @@ console.log({
   pid,
   input,
 });
+
+h1.innerHTML = "Escribiendo desde <br>JavaScript";
+h1.innerText = "Escribiendo desde <br>JavaScript";
+console.log(h1.getAttribute("pantalla"));
+console.log(h1.getAttribute("class"));
+h1.setAttribute("class", "rojo");
+console.log(h1.getAttribute("class"));
+
+h1.classList.add("amarillo");
+console.log(h1.getAttribute("class"));
+h1.classList.remove("rojo");
+console.log(h1.getAttribute("class"));
+// h1.classList.toggle("rojo");
+// h1.classList.contains("rojo");
+
+input.value = "456";
+
+const img = document.createElement("img");
+img.setAttribute(
+  "src",
+  "https://www.geekmi.news/__export/1641156098758/sites/debate/img/2022/01/02/disexo_-_2022-01-02t134133_864.jpg_242310155.jpg"
+);
+
+console.log(img);
+pid.innerHTML = "";
+pid.append(img);
