ffd838e3190fd3afbc9c1eb26dcdae4309e3a9db
Héctor Airy <hola@hectorairy.com>
2022-07-30 18:07:57 -0500

    use addEventListener

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const calculo1 = document.querySelector("#calculo1");
 const calculo2 = document.querySelector("#calculo2");
 const btn = document.querySelector("#btnCalcular");
 const resultado = document.querySelector("#resultado");
+const form = document.querySelector("#formulario");
 
-function btnOnClick() {
+// btn.addEventListener("click", btnOnClick);
+form.addEventListener("submit", sumar);
+
+function sumar(event) {
+  event.preventDefault();
   const suma = Number(calculo1.value) + Number(calculo2.value);
   resultado.innerText = "Resultado: " + suma;
 }
